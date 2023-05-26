49b5bcc1b0853d4c6dcd3c3e93e9187cbd18b8f1
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2023-05-25 23:30:30 -0300

    home fixes

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+/* const pool = new Pool({
   connectionString:
     "postgres://postgres:RDq6d19axLHjfWY@rkeventos-db.flycast:5432",
-});
+}); */
 
-//const pool = new Pool();
+const pool = new Pool();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
